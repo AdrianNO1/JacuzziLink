0719e44a75c3b9b3f3346f8e34816fcadeaa9b87
Adrian <adrianrosten@gmail.com>
2025-02-08 16:36:34 +0100

    PWA fix

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,52 @@
+const CACHE_NAME = 'hottubhub-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-400x400.jpg',
+    'https://unpkg.com/mqtt/dist/mqtt.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js'
+];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('pwa-cache').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './icon-400x400.jpg'
-            ]);
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
         })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then((response) => {
+                    // Don't cache MQTT responses
+                    if (!response || response.status !== 200 || response.type !== 'basic' ||
+                        event.request.url.includes('mosquitto')) {
+                        return response;
+                    }
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                });
+            })
     );
 });
